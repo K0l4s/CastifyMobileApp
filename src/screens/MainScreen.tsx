88c343461a355a731f69cad6329dec77ed65d916
nauht1 <thuanngo454@gmail.com>
2025-01-28 15:09:53 +0700

    Update swipeable bottom tab navigator

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -8,19 +8,42 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Text } from 'react-native';
 
 enableScreens();
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const MainScreen = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Tab.Navigator
+        tabBarPosition="bottom"
         screenOptions={({ route }) => ({
           headerShown: false,
           tabBarShowLabel: true,
           tabBarStyle: { backgroundColor: '#fff' },
+          tabBarLabel: ({ focused, color }) => {
+            let label;
+            if (route.name === 'Home') {
+              label = 'Home';
+            } else if (route.name === 'Following') {
+              label = 'Following';
+            } else if (route.name === 'Create') {
+              label = 'Create';
+            } else if (route.name === 'Chat') {
+              label = 'Chat';
+            } else if (route.name === 'Inbox') {
+              label = 'Inbox';
+            }
+
+            return (
+              <Text style={{ fontSize: 10, color: focused ? '#0c0461' : 'gray' }}>
+                {label}
+              </Text>
+            );
+          },
           tabBarIndicatorStyle: { backgroundColor: '#0c0461' }, // Đường gạch dưới tab
           tabBarIcon: ({ focused, color }) => {
             let iconName;
@@ -37,7 +60,7 @@ const MainScreen = () => {
               iconName = focused ? 'mail' : 'mail-outline';
             }
 
-            return <Icon name={iconName} size={22} color={focused ? '#0c0461' : 'gray'} />;
+            return <Icon name={iconName} size={20} color={focused ? '#0c0461' : 'gray'} />;
           },
         })}
       >
