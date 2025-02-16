9031d6b2fa9e7f950cfafd1b2675433ec98ef5d4
nauht1 <thuanngo454@gmail.com>
2025-02-16 14:27:42 +0700

    add PodcastScreen

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import Toast from 'react-native-toast-message';
 import ProfileScreen from './src/screens/ProfileScreen';
+import PodcastScreen from './src/screens/PodcastScreen';
 
 
 const Stack = createStackNavigator<RootParamList>();
@@ -21,6 +22,7 @@ const App = () => {
           <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Main" component={MainScreen} />
           <Stack.Screen name="Profile" component={ProfileScreen}/>
+          <Stack.Screen name="Podcast" component={PodcastScreen}/ >
         </Stack.Navigator>
         <Toast />
       </NavigationContainer>
