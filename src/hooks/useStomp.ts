f9ea5377954e4581eb4ff5d051d18a180e03ebb5
Huỳnh Trung Kiên <21110223@student.hcmute.edu.vn>
2025-02-19 02:39:00 +0700

    update: config hook useStomp

--- src/hooks/useStomp.ts
+++ src/hooks/useStomp.ts
@@ -1,73 +1,77 @@
-// import { useEffect, useRef, useState } from 'react';
-// import { Client, Message } from '@stomp/stompjs';
-// import SockJS from 'sockjs-client';
+import { useEffect, useRef, useState } from 'react';
+import { Client, Message } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 // import { getItem } from '@react-native-async-storage/async-storage';
-// import { BaseApi } from '../utils/axiosInstance';
+import { BaseApi } from '../utils/axiosInstance';
+import * as Keychain from "react-native-keychain";
 
-// interface UseStompProps {
-//     subscribeUrl: string;
-//     trigger: any[];
-//     flag?: boolean;
-//     onDisconnect?: () => void;
-//     onConnect?: () => void;
-//     onStompError?: (frame: any) => void;
-//     onWebSocketError?: (error: any) => void;
-//     reconnectDelay?: number;
-// }
+interface UseStompProps {
+    subscribeUrl: string;
+    trigger: any[];
+    flag?: boolean;
+    onDisconnect?: () => void;
+    onConnect?: () => void;
+    onStompError?: (frame: any) => void;
+    onWebSocketError?: (error: any) => void;
+    reconnectDelay?: number;
+}
 
-// const useStomp = ({ subscribeUrl, trigger, flag = true, onConnect, onDisconnect, onStompError, onWebSocketError, reconnectDelay }: UseStompProps) => {
-//     const stompClientRef = useRef<Client | null>(null);
-//     const [object, setObject] = useState<any>(null);
+const useStomp = ({ subscribeUrl, trigger, flag = true, onConnect, onDisconnect, onStompError, onWebSocketError, reconnectDelay }: UseStompProps) => {
+    const stompClientRef = useRef<Client | null>(null);
+    const [object, setObject] = useState<any>(null);
 
-//     useEffect(() => {
-//         const initializeWebSocket = async () => {
-//             const token = await getItem('token');
-//             const socket = new SockJS(BaseApi + "/ws");
-//             const stompClient = new Client({
-//                 webSocketFactory: () => socket,
-//                 reconnectDelay: reconnectDelay ? reconnectDelay : 5000,
-//                 connectHeaders: {
-//                     Authorization: `Bearer ${token}`,
-//                 },
-//                 onConnect: () => {
-//                     if (flag) {
-//                         stompClient.subscribe(subscribeUrl, (message: any) => {
-//                             const newMessage: Message = JSON.parse(message.body);
-//                             setObject(newMessage);
-//                         });
-//                     }
-//                     onConnect && onConnect();
-//                 },
-//                 onDisconnect: () => {
-//                     console.log("❎ WebSocket disconnected");
-//                     onDisconnect && onDisconnect();
-//                 },
-//                 onStompError: (frame: any) => {
-//                     console.error("🚨 Broker reported error: " + frame.headers["message"]);
-//                     console.error("📄 Additional details: " + frame.body);
-//                     onStompError && onStompError(frame);
-//                 },
-//                 onWebSocketError: (error: any) => {
-//                     console.error("🔌 WebSocket error:", error);
-//                     onWebSocketError && onWebSocketError(error);
-//                 },
-//             });
+    useEffect(() => {
+        const initializeWebSocket = async () => {
+            // const token = await getItem('token');
+                  const credentials = await Keychain.getGenericPassword(); // get token from Keychain
+            const tokenData = credentials ? JSON.parse(credentials.password) : null;
+            const token = tokenData.access_token;
+            const socket = new SockJS(BaseApi + "/ws");
+            const stompClient = new Client({
+                webSocketFactory: () => socket,
+                reconnectDelay: reconnectDelay ? reconnectDelay : 5000,
+                connectHeaders: {
+                    Authorization: `Bearer ${token}`,
+                },
+                onConnect: () => {
+                    if (flag) {
+                        stompClient.subscribe(subscribeUrl, (message: any) => {
+                            const newMessage: Message = JSON.parse(message.body);
+                            setObject(newMessage);
+                        });
+                    }
+                    onConnect && onConnect();
+                },
+                onDisconnect: () => {
+                    console.log("❎ WebSocket disconnected");
+                    onDisconnect && onDisconnect();
+                },
+                onStompError: (frame: any) => {
+                    console.error("🚨 Broker reported error: " + frame.headers["message"]);
+                    console.error("📄 Additional details: " + frame.body);
+                    onStompError && onStompError(frame);
+                },
+                onWebSocketError: (error: any) => {
+                    console.error("🔌 WebSocket error:", error);
+                    onWebSocketError && onWebSocketError(error);
+                },
+            });
 
-//             stompClient.activate();
-//             stompClientRef.current = stompClient;
-//         };
+            stompClient.activate();
+            stompClientRef.current = stompClient;
+        };
 
-//         initializeWebSocket();
+        initializeWebSocket();
 
-//         return () => {
-//             console.log("🔄 Cleaning up WebSocket...");
-//             if (stompClientRef.current) {
-//                 stompClientRef.current.deactivate();
-//             }
-//         };
-//     }, [...trigger]);
+        return () => {
+            console.log("🔄 Cleaning up WebSocket...");
+            if (stompClientRef.current) {
+                stompClientRef.current.deactivate();
+            }
+        };
+    }, [...trigger]);
 
-//     return object;
-// };
+    return object;
+};
 
-// export default useStomp;
+export default useStomp;
