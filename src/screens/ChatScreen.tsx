1c70cfe385aef33b13c32a7d556c0b74be599cda
Huỳnh Trung Kiên <21110223@student.hcmute.edu.vn>
2025-02-24 05:50:22 +0700

    feat: message non realtime

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { shortConversation } from '../models/Conversation';
 import { conversationService } from '../services/conversationService';
 import DateUtil from '../utils/dateUtil';
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux/store';
-import useStomp from '../hooks/useStomp';
-import { BaseApi } from '../utils/axiosInstance';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootParamList } from '../type/navigationType';
 
-interface MessageData {
-  message: string;
-}
 const ChatScreen = () => {
   const [chatData, setChatData] = useState<shortConversation[]>([]);
+  const navigation = useNavigation<StackNavigationProp<RootParamList, 'ChatScreen'>>();
+
   const fetchData = async () => {
     const response = await conversationService.getByUserId(0, 10);
     setChatData(response.data.data);
-  }
-  useEffect(() => {
+  };
 
+  useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
+
   return (
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
-        <Text style={styles.createNewText}> CREATE NEW CONVERSATION</Text>
+        <Text style={styles.createNewText}>CREATE NEW CONVERSATION</Text>
       </View>
 
       {/* Chat List */}
@@ -33,17 +32,25 @@ const ChatScreen = () => {
         data={chatData}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View style={styles.chatItem}>
-            <Image source={{ uri: item.imageUrl? item.imageUrl : "https://png.pngtree.com/element_our/png_detail/20180904/group-avatar-icon-design-vector-png_75950.jpg" }} style={styles.avatar} />
+          <TouchableOpacity 
+            style={styles.chatItem} 
+            onPress={() => navigation.navigate('ChatDetailScreen', { conversationId: item.id })}
+          >
+            <Image 
+              source={{ uri: item.imageUrl || "https://png.pngtree.com/element_our/png_detail/20180904/group-avatar-icon-design-vector-png_75950.jpg" }} 
+              style={styles.avatar} 
+            />
             <View style={styles.chatDetails}>
               <Text style={styles.groupName}>{item.title}</Text>
               <Text style={[styles.lastMessage && styles.unreadMessage]}>
-                {item.lastMessage?.sender.fullname + ":" + item.lastMessage?.content}
+                {item.lastMessage?.sender.fullname + ": " + item.lastMessage?.content}
               </Text>
             </View>
-            <Text style={styles.time}>{item.lastMessage?.timestamp &&
-              DateUtil.formatDateToTimeAgo(item.lastMessage?.timestamp ? new Date(item.lastMessage?.timestamp) : new Date())}</Text>
-          </View>
+            <Text style={styles.time}>
+              {item.lastMessage?.timestamp &&
+                DateUtil.formatDateToTimeAgo(new Date(item.lastMessage?.timestamp))}
+            </Text>
+          </TouchableOpacity>
         )}
       />
     </View>
