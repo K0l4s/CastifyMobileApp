a5830a0530c33cd0147f576ad0019158dd2b966e
The Thanh Nguyen <thanhnt932@gmail.com>
2025-03-12 01:08:07 +0700

    search function

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,7 @@
 import { updateUser } from "../models/User";
-import { axiosInstanceAuth, axiosInstanceFile } from "../utils/axiosInstance";
+import { PodcastResponse } from "../models/PodcastModel";
+import { userCard } from "../models/User";
+import { axiosInstance, axiosInstanceAuth, axiosInstanceFile, BaseApi } from "../utils/axiosInstance";
 
 class UserService {
   static async getUserByToken() {
@@ -20,6 +22,60 @@ class UserService {
     const formData = new FormData();
     formData.append('avatar', avatar);
     return await axiosInstanceFile.put(`/api/v1/user/avatar`, formData);
+  }
+
+  
+  // static async searchPodcasts(keyword: string, pageNumber = 0, pageSize = 10) {
+  //   try {
+  //     const response = await axiosInstance.get<PodcastResponse>("/api/v1/search/post", {
+  //       params: {
+  //         pageNumber,
+  //         pageSize,
+  //         keyword
+  //       }
+  //     });
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error("Error searching podcasts:", error);
+  //     throw error;
+  //   }
+  // }
+
+  // static async searchUsers(keyword: string, pageNumber = 0, pageSize = 10) {
+  //   // try {
+  //     // const response = await axiosInstance.get<userCard>("/api/v1/search/user", {
+  //     //   params: {
+  //     //     pageNumber,
+  //     //     pageSize,
+  //     //     keyword
+  //     //   }
+  //     return axiosInstance.get(`/api/v1/search/user?pageNumber=${pageNumber}&pageSize=${pageSize}&keyword=${keyword}`);
+  //   //   });
+  //   //   return response.data;
+  //   // } catch (error) {
+  //   //   console.error("Error searching users:", error);
+  //   //   throw error;
+  //   // }
+  // }
+  
+  static async searchUsers(keyword: string, pageNumber = 0, pageSize = 10) {
+    try {
+      const response = await axiosInstance.get(`/api/v1/search/user?pageNumber=${pageNumber}&pageSize=${pageSize}&keyword=${keyword}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error searching users:", error);
+      throw error;
+    }
+  }
+
+  static async searchPodcasts(keyword: string, pageNumber = 0, pageSize = 10) {
+    try {
+      const response = await axiosInstance.get(`/api/v1/search/post?pageNumber=${pageNumber}&pageSize=${pageSize}&keyword=${keyword}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error searching podcasts:", error);
+      throw error;
+    }
   }
 }
 
